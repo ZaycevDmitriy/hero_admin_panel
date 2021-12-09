afb6f23acc3673e73174e0c994e485185253ebe8
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-12-09 22:54:49 +0300

    Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,34 @@
-import { combineReducers, createStore, compose } from 'redux';
+import { combineReducers, compose, applyMiddleware, createStore } from 'redux';
 import { filters, heroes } from '../reducers';
 
-const enhancer = (createStore) => (...args) => {
-  const store = createStore(...args);
-
-  const oldDispatch = store.dispatch;
-  store.dispatch = (action) => {
-    if (typeof action === 'string') {
-      return oldDispatch({ type: action });
-    } else {
-      return oldDispatch(action);
-    }
-  };
-  return store;
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({ type: action });
+  } else {
+    return next(action);
+  }
 };
+
+// const enhancer = (createStore) => (...args) => {
+//   const store = createStore(...args);
+//
+//   const oldDispatch = store.dispatch;
+//   store.dispatch = (action) => {
+//     if (typeof action === 'string') {
+//       return oldDispatch({ type: action });
+//     } else {
+//       return oldDispatch(action);
+//     }
+//   };
+//   return store;
+// };
 
 const store = createStore(
                             combineReducers({ heroes, filters }),
                             compose(
-                              enhancer,
+                              applyMiddleware(stringMiddleware),
                               window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                            )
+                              )
                           );
 
 export default store;
